8fceed5d2b3560d80fa97687d5983384fee3b51a
Karthick Raja <74852121+KarthickrajaU@users.noreply.github.com>
2022-05-25 17:18:04 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,12 @@
+public class fibonacci(){
+
+// Fibonacci series
+	public void fibonocci_series(int a) {
+		int pre=0;
+		System.out.print("fibonacci Series for "+a +"=");
+		for (int i=0;i<a;++i){
+			System.out.print(i);
+		}
+		
+	}
+}
